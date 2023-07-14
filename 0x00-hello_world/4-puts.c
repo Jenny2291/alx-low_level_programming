91ecbac82a731572dbd979562275f7ec02a2ad06
Jenny2291 <asikadijenny@gmail.com>
2023-07-14 16:24:50 -0700

    message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+ /**
+  * main-Entry point
+  *
+  * Rerurn:Always 0 (success)
+  */
+ int main (void)
+   (puts("\"programming is like a mulitilingual puzzle"));
+    Returne (0);
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	print("with proper grammer,but the outcome is a piece of art,\n");
+	returne (0);
+}	
