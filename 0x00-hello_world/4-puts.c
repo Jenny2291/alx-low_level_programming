16e737fbed2f73c079db216c6d5791d1ade6a536
Jenny2291 <129335315+Jenny2291@users.noreply.github.com>
2023-07-17 00:45:03 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,8 @@
   *
   * Rerurn:Always 0 (success)
   */
- int main (void)
-   (puts("\"programming is like a mulitilingual puzzle"));
+ int main(void)
+{
+   puts("\"programming is like a mulitilingual puzzle"));
     Returne (0);
+}
