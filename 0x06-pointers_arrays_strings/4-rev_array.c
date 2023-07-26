e522a175650db0805ec59d21d4c5356b90d1a548
Jenny2291 <129335315+Jenny2291@users.noreply.github.com>
2023-07-26 22:51:01 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,44 @@
+
 #include "main.h"
+#include <stdio.h>
+
 /**
- * reverse_array - reverses the content of an array
- * of integers.
- * @a: array.
- * @n: number of elements of the array.
+ * print_buffer - prints a buffer
+ * @b: buffer.
+ * @size: size of buffer.
  * Return: no return.
  */
-
-void reverse_array(int *a, int n)
+void print_buffer(char *b, int size)
 {
-	int i, j, temp;
+	int j, k, l;
 
-	for (i = 0; i < n - 1; i++)
+	if (size <= 0)
+		printf("\n");
+	else
 	{
-		for (j = i + 1; j > 0; j--)
+		for (j = 0; j < size; j += 10)
 		{
-			temp = *(a + j);
-			*(a + j) = *(a + (j - 1));
-			*(a + (j - 1)) = temp;
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
 		}
 	}
+}
