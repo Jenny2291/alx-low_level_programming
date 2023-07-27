55edaee3831873d0ab53b9a2209c1d10f46dcf72
Jenny2291 <129335315+Jenny2291@users.noreply.github.com>
2023-07-27 22:52:58 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,39 +1,23 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array
+ * of integers.
+ * @a: array.
+ * @n: number of elements of the array.
+ * Return: no return.
+ */
 
-/* Function to reverse the content of an array of integers */
 void reverse_array(int *a, int n)
 {
-    int temp, i, j;
+	int i, j, temp;
 
-    for (i = 0, j = n - 1; i < j; i++, j--)
-    {
-        /* Swap elements a[i] and a[j] */
-        temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-}
-
-int main(void)
-{
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: ");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    reverse_array(arr, n);
-
-    printf("Reversed array: ");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    return 0;
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
 }
