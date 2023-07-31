cc07cd2f97fe2f00fbbed56ae0c384526aec77af
Jenny2291 <asikadijenny@gmail.com>
2023-07-31 13:09:33 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+/* _putchar.c */
+
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (stdout).
+ * @c: The character to be written.
+ *
+ * Return: On success, the number of characters written is returned.
+ *         On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
+
