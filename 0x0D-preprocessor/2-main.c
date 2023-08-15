6760bd0959300ba0fb15351dcc6cd29c2b11cb0d
Jenny2291 <129335315+Jenny2291@users.noreply.github.com>
2023-08-16 00:48:19 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,11 @@
-#include <stdud.h>
-
+#include <stdio.h>
 /**
- * main -prints the name of the file
- *
- * Returne: Always 0 (success)
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
  */
+
 int main(void)
 {
-	printf("%s\n" ,__FILE__);
-	return (0)
+printf("%s\n", __FILE__);
+return (0);
 }
