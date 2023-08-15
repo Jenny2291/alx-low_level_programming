546a4d9c48d02b3acf184f4be054c433a16e40b9
Jenny2291 <asikadijenny@gmail.com>
2023-08-15 16:07:15 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdud.h>
+
+/**
+ * main -prints the name of the file
+ *
+ * Returne: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n" ,__FILE__);
+	return (0)
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define ABC(x ((x) < (0)? (x) : (x))
+
+#endif
