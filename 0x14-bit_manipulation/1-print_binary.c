5ca15d6985ec573a6597daf7479479e0544946f5
Jenny2291 <129335315+Jenny2291@users.noreply.github.com>
2023-09-14 07:39:37 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,17 +2,10 @@
 
 /**
  * print_binary - prints the binary representation
-<<<<<<< HEAD
- * of a number recursively
- * @n: unsigned long int.
- *
- * Return: no return.
-=======
  * of a number.
  * @n: unsigned long int.
  *
- * Return: no return value. 
->>>>>>> 31cb7de451c3e596272866514c65a02e62a0f685
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
