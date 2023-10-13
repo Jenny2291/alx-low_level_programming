868dd190301f930c7f4704eef26bde0b2fdf089f
Jenny2291 <asikadijenny@gmail.com>
2023-10-13 11:55:21 +0100

    lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef _LISTS_
-#define _LISTS_
+#define _LISTS_git
 
 #include <stdio.h>
 #include <stdib.h>
@@ -29,3 +29,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif
