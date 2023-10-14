f8ea2aab5e6b71c0cbe12a1674787e731601414d
Jenny2291 <129335315+Jenny2291@users.noreply.github.com>
2023-10-14 01:43:37 +0100

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef _LISTS_
-#define _LISTS_git
+#define _LISTS_
 
 #include <stdio.h>
-#include <stdib.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -11,13 +11,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
