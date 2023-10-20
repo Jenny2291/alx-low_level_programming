0eaa59bc7605305502b675193211788cfac6ff25
Jenny2291 <asikadijenny@gmail.com>
2023-10-20 19:14:39 +0100

    dynamic-libraries

---
+++ 0x18-dynamic_libraries/0-print_funcs.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _putchar - prints character to stdout
+ * @c: charcter to print
+ * Return: character
+*/
+int _putchar(char c)
+{
+}
+
+/**
+ * _puts - prints string to stdout
+ * @s: points to string to print
+*/
+void _puts(char *s)
+{
+}
+
+/**
+ * _abs - prints the absolute value of an int
+ * @n: integer in question
+ * Return: Absplute value of int
+*/
+int _abs(int n)
+{
+}
+
+/**
+ * _atoi - converts string to int
+ * @s: points to string to check
+ * Return: int
+*/
+int _atoi(char *s)
+{
+}
